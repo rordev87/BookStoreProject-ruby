e8f5cfe00bd2107c276bb21a8120775d62e36733
rordev87 <rordev87@proton.me>
2016-04-28 16:48:49 +0000

    Search only on book title

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,6 @@ class Book < ActiveRecord::Base
     belongs_to :author
     
     def self.search(search)
-        where("title LIKE ?", "%#{search}%") 
-        where("description LIKE ?", "%#{search}%")
+        where("title LIKE ?", "%#{search}%")
     end
 end
