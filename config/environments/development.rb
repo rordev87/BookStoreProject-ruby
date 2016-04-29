366231ca432768a61d74154f76685df049776b55
rordev87 <rordev87@proton.me>
2016-04-29 19:33:39 +0000

    Fix action mailer addresses

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'rails-tutorial-kevkevd.c9users.io'
+  host = 'ruby-primes-shonagh.c9users.io'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
